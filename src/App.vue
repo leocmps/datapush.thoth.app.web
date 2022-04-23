31c663255f10641079bfbdd5b80eeaa8fa42fff6
Leonardo Campos <64384566+leocmps@users.noreply.github.com>
2022-04-23 15:55:11 -0300

    Adicionando bootstrap no projeto e iniciando nav-bar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,85 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      class="mx-16"
+      color="transparent"
+      elevation="0"
+    >
+      <img
+        src="./assets/logo.png"
+        width="50"
+      >
+      <h1 class="ml-2 title">
+        THOTH.
+      </h1>
+
+      <v-spacer />
+
+      <div class="mr-16">
+        Início
+      </div>
+      <div class="mr-16">
+        Cursos
+      </div>
+      <div class="mr-16">
+        Aulas
+      </div>
+      <div class="mr-16">
+        Fóruns
+      </div>
+
+      <v-menu
+        close-on-content-click
+        nudge-bottom="5"
+        nudge-left="40"
+        offset-y
+        rounded="lg"
+        transition="scroll-x-reverse-transition"
+      >
+        <template #activator="{ on }">
+          <v-btn
+            color="white"
+            elevation="0"
+            rounded="lg"
+            v-on="on"
+          >
+            <v-icon size="22">
+              mdi-menu
+            </v-icon>
+            <v-icon size="28">
+              mdi-account-circle
+            </v-icon>
+          </v-btn>
+        </template>
+        <v-card rounded="lg">
+          <v-list dense>
+            <v-list-item>Cadastrar-se</v-list-item>
+            <v-list-item>Entrar</v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.v-application {
+  background: #EEFDF2 !important;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.title {
+  font-weight: 800;
 }
 </style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>1</div>
+</template>
+
+<script lang="ts" src="./Home.ts"></script>
